4a7b5b74b5094f497a743dee90c249da6095901e
Omer <engromerbaig@gmail.com>
2025-04-29 13:12:38 +0500

    data increased

--- src/components/Hero/modules/logoData.js
+++ src/components/Hero/modules/logoData.js
@@ -8,6 +8,14 @@ const logoData = [
   { id: 2, image: logo2 },
   { id: 3, image: logo3 },
   { id: 4, image: logo4 },
+  { id: 5, image: logo1 },
+  { id: 6, image: logo2 },
+  { id: 7, image: logo3 },
+  { id: 8, image: logo4 },
+  { id: 9, image: logo1 },
+  { id: 10, image: logo2 },
+  { id: 11, image: logo3 },
+  { id: 12, image: logo4 },
 ];
 
 export default logoData;
