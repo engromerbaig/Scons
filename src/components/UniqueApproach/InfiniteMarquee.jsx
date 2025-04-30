af21e741a90a88469ed1813f2b385366f9aedfc3
Omer <engromerbaig@gmail.com>
2025-04-30 13:16:33 +0500

    almost there

---
+++ src/components/UniqueApproach/InfiniteMarquee.jsx
@@ -0,0 +1,25 @@
+import Marquee from "react-fast-marquee";
+import Heading from "../Heading/Heading";
+
+const InfiniteMarquee = ({ items }) => (
+  <Marquee gradient={false} speed={50} pauseOnHover={true}>
+    {items.map((topic, idx) => (
+      <span key={topic + idx} className="flex items-center">
+        <Heading
+          text={topic}
+          color="text-black"
+          spanColor="text-black"
+          fontWeight="font-medium"
+          size="text-60px"
+          className="whitespace-nowrap"
+        />
+        {/* Add bullet unless last item */}
+        {idx !== items.length - 1 && (
+          <span className="mx-8 font-black text-50px text-black select-none">•</span>
+        )}
+      </span>
+    ))}
+  </Marquee>
+);
+
+export default InfiniteMarquee;
--- src/components/UniqueApproach/UniqueApproachOld.jsx
+++ src/components/UniqueApproach/UniqueApproachOld.jsx
@@ -1,9 +1,9 @@
+import InfiniteMarquee from "./InfiniteMarquee";
+
 import Heading from "../Heading/Heading";
 import BodyText from "../BodyText/BodyText";
 import { theme } from "../../theme";
 import Highlights from "./modules/Highlights";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css"; // Basic CSS
 
 const itTopics = [
   "Web Design",
@@ -18,71 +18,38 @@ const itTopics = [
   "Blockchain"
 ];
 
-const UniqueApproachOld = () => {
+const UniqueApproach = () => {
   return (
     <div
       className={`flex flex-col justify-center overflow-hidden bg-white ${theme.layoutPages.paddingVertical}`}
     >
-
       <div className={`${theme.layoutPages.paddingHorizontal} `}>
-      <div className="relative w-full">
-        <Heading
-          text="Our Unique Approach"
-          color="text-black"
-          spanColor="text-black"
-          spanText="Approach"
-          centered={false}
-        />
+        <div className="relative w-full">
+          <Heading
+            text="Our Unique Approach"
+            color="text-black"
+            spanColor="text-black"
+            spanText="Approach"
+            centered={false}
+          />
+        </div>
+        <div className="relative w-full">
+          <BodyText
+            text="At Tyfora, innovation meets precision. Our client-centric approach ensures we understand your vision, ideate effectively, and deliver solutions that exceed expectations. From concept to deployment, we bring expertise and passion to every project."
+            color="text-black"
+            centered={false}
+          />
+        </div>
+        <div className={`relative ${theme.layoutPages.paddingVerticalTop} px-0 w-full`}>
+          <Highlights />
+        </div>
       </div>
-
-      <div className="relative w-full">
-        <BodyText
-          text="At Tyfora, innovation meets precision. Our client-centric approach ensures we understand your vision, ideate effectively, and deliver solutions that exceed expectations. From concept to deployment, we bring expertise and passion to every project."
-          color="text-black"
-          centered={false}
-        />
-      </div>
-
-      <div className={`relative ${theme.layoutPages.paddingVerticalTop} px-0 w-full`}>
-        <Highlights />
-      </div>
-
-      </div>
-  
 
       <div className="mt-4">
-        <Splide
-          options={{
-            type: "loop",
-            perPage: 4,
-            gap: "1rem",
-            pagination: true,
-            arrows: false,
-            autoplay: true,
-            interval: 3000,
-            breakpoints: {
-              768: { perPage: 1 },
-              1024: { perPage: 2 }
-            }
-          }}
-        >
-          {itTopics.map((topic, index) => (
-            <SplideSlide key={index}>
-              <div className="">
-                <Heading
-                text={topic}
-                color="text-black"
-                spanColor="text-black"
-                fontWeight="font-medium"
-                size="text-60px"
-                />
-              </div>
-            </SplideSlide>
-          ))}
-        </Splide>
+        <InfiniteMarquee items={itTopics} />
       </div>
     </div>
   );
 };
 
-export default UniqueApproachOld;
+export default UniqueApproach;
