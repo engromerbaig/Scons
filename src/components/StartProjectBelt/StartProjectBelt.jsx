4c1bcea8549cbfdd937069285e085122d3cc4788
Omer <engromerbaig@gmail.com>
2025-05-03 16:26:15 +0500

    ok

--- src/components/StartProjectBelt/StartProjectBelt.jsx
+++ src/components/StartProjectBelt/StartProjectBelt.jsx
@@ -8,7 +8,7 @@ const StartProjectBelt = () => {
     return ( <div className={`bg-white ${theme.layoutPages.paddingHorizontal} ${theme.layoutPages.paddingVertical} `}>
 
 
-        <div className=" bg-grayBg rounded-full shadow-md border-[0.1px] border-gray-300 flex flex-row items-center justify-between py-4 px-10">
+        <div className=" bg-grayBg rounded-full shadow-md border-[0.1px] border-gray-100 flex flex-row items-center justify-between py-4 px-10">
 
         <Heading text="Ready to Start Your Project?" spanText="Project?" spanColor="text-neon" size="text-40px" color="text-black" centered={false} />
     
