4263072d7a1b813b4826ddc594f0fecfb15956ca
Omer <engromerbaig@gmail.com>
2025-05-03 13:57:16 +0500

    belt

--- src/components/StartProjectBelt/StartProjectBelt.jsx
+++ src/components/StartProjectBelt/StartProjectBelt.jsx
@@ -5,18 +5,24 @@ import Button from "../Button/Button";
 import { Link } from "react-router-dom";
 
 const StartProjectBelt = () => {
-    return ( <div className={`px-10 py-4 border-1 border-gray-500 rounded-full shadow-md my-20 mx-40 flex flex-row items-center justify-between  bg-grayBg`}>
+    return ( <div className={`bg-white ${theme.layoutPages.paddingHorizontal} ${theme.layoutPages.paddingVertical} `}>
 
-       <Heading text="Ready to Start Your Project?" spanText="Project?" spanColor="text-neon" size="text-40px" color="text-black" centered={false} />
+
+        <div className=" bg-grayBg rounded-full shadow-md border-[0.1px] border-gray-300 flex flex-row items-center justify-between py-4 px-10">
+
+        <Heading text="Ready to Start Your Project?" spanText="Project?" spanColor="text-neon" size="text-40px" color="text-black" centered={false} />
     
-          <Link to="#">
-          <Button
-  name="Get Started"
-  bgColor="bg-black"
-  textColor="white"
- 
-  className="px-4 py-2"
+    <Link to="#">
+    <Button
+name="Get Started"
+bgColor="bg-black"
+textColor="white"
+
+className="px-4 py-2"
 />       </Link>
+        </div>
+
+     
 
     </div> );
 }
