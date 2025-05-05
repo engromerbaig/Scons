95d4c163a1657c5665dc5185fced0b3fac2c0a62
Omer <engromerbaig@gmail.com>
2025-05-05 15:49:52 +0500

    ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ import { theme } from "../../theme";
 
 const Footer = () => {
   return (
-    <footer className={`w-full bg-black text-white ${theme.layoutPages.paddingHorizontal} ${theme.layoutPages.paddingVertical}`}>
+    <footer className={`w-full bg-black text-white min-h-screen ${theme.layoutPages.paddingHorizontal} ${theme.layoutPages.paddingVerticalTop}`}>
       
       {/* ROW 1: Logo + Contact | Pages | Services */}
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-12 border-b border-gray-800 pb-10 text-left">
