fbf63bf8fb7f63b4712cb7055d85f8144307eaeb
Omer <engromerbaig@gmail.com>
2025-05-07 14:47:01 +0500

    better

--- src/components/Vision/visionMissionData.js
+++ src/components/Vision/visionMissionData.js
@@ -1,8 +1,12 @@
 // data/visionMissionData.js
 
-import visionImg from "../../assets/images/about/vision.webp";
+import visionImg from "../../assets/images/vision/1.webp";
 
-import missionImg from "../../assets/images/about/mission.webp";
+import missionImg from "../../assets/images/vision/2.webp";
+import customerImg from "../../assets/images/vision/3.webp";
+import transparencyImg from "../../assets/images/vision/4.webp";
+
+
 
 export const visionMissionData = [
   {
@@ -16,5 +20,19 @@ export const visionMissionData = [
     span: "Mission",
     body: "To deliver cutting-edge software solutions that solve real-world problems, drive business growth, and anticipate the challenges of tomorrow.",
     imageSrc: missionImg,
+  },
+
+  {
+    heading: "Customer First",
+    span: "First",
+    body: "To deliver cutting-edge software solutions that solve real-world problems, drive business growth, and anticipate the challenges of tomorrow.",
+    imageSrc: customerImg,
+  },
+
+  {
+    heading: "Transparency",
+    span: "Transparency",
+    body: "To deliver cutting-edge software solutions that solve real-world problems, drive business growth, and anticipate the challenges of tomorrow.",
+    imageSrc: transparencyImg,
   },
 ];
