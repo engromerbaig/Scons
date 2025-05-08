ddd690ec960b4a01c6ba5c95fe71363ef051b06f
Omer <engromerbaig@gmail.com>
2025-05-08 17:23:02 +0500

    easy

--- src/pages/OurWork/FilterControls.jsx
+++ src/pages/OurWork/FilterControls.jsx
@@ -32,7 +32,7 @@ const FilterControls = ({
       {/* Dropdown Filters */}
       <div className="flex flex-wrap gap-4 items-center">
         <select
-          className="px-4 py-2 border rounded-md text-sm"
+          className="px-10 py-4  rounded-full bg-grayBg text-sm"
           value={selectedTechnology}
           onChange={(e) => handleTechnologyChange(e.target.value)}
         >
