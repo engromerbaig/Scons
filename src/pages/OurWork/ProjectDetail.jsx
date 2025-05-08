a90a02e12adbf690880accb2a5e43fab8f442355
Omer <engromerbaig@gmail.com>
2025-05-08 18:18:29 +0500

    sd

--- src/pages/OurWork/ProjectDetail.jsx
+++ src/pages/OurWork/ProjectDetail.jsx
@@ -21,8 +21,23 @@ const ProjectDetail = () => {
     <div
       className={` ${theme.layoutPages.paddingVertical} min-h-screen`}
     >
+
+      <div className={` ${theme.layoutPages.paddingHorizontal} flex flex-col items-start justify-center gap-y-2`}>
+      <img src={project.logo} alt={`${project.heading} logo`} className="w-1/5 " />
+
+        <Heading text={project.heading} className="text-left" />
+
+        <div className="border-2 border-black rounded-full px-2 py-1">
+        <BodyText
+        text={project.service}
+        size="text-xs"
+        fontWeight="font-semibold"
+        />
+
+        </div>
+
+        </div>
       <div className="flex flex-col gap-4">
-      <img src={project.logo} alt={`${project.heading} logo`} className="w-1/4 mb-4" />
       <Heading text={project.heading} size="text-35px" fontWeight="font-semibold" centered={false} />
         <img src={project.coverImage} alt={project.heading} className="w-full h-[600px] object-cover rounded-3xl mb-4" />
 
