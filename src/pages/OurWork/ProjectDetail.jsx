c6394c66af68b588409382642fa56bf74fecc482
Omer <engromerbaig@gmail.com>
2025-05-09 17:45:59 +0500

    added

--- src/pages/OurWork/ProjectDetail.jsx
+++ src/pages/OurWork/ProjectDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { theme } from "../../theme";
 import projects from "./projectDetails";
 import Heading from "../../components/Heading/Heading";
@@ -16,7 +16,6 @@ const ProjectDetail = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  // Function to find technology icon
   const findTechnologyIcon = (techName) => {
     for (const category in technologiesData) {
       for (const platform in technologiesData[category]) {
@@ -35,29 +34,14 @@ const ProjectDetail = () => {
 
   return (
     <div className={`${theme.layoutPages.paddingVertical} min-h-screen`}>
-      <div
-        className={`${theme.layoutPages.paddingHorizontal} flex flex-col items-start justify-center gap-y-2`}
-      >
-        <img
-          src={project.logo}
-          alt={`${project.heading} logo`}
-          className="w-1/5"
-        />
+      {/* Header Section */}
+      <div className={`${theme.layoutPages.paddingHorizontal} flex flex-col gap-y-4`}>
+        <img src={project.logo} alt={`${project.heading} logo`} className="w-1/5" />
 
-        <Heading
-          text={project.heading}
-          centered={false}
-          className="text-left leading-tight"
-        />
-        <Heading
-          text={project.headline}
-          centered={false}
-          size="text-40px"
-          fontWeight="font-medium"
-          className="text-left leading-tight"
-        />
+        <Heading text={project.heading} centered={false} className="text-left" lineHeight="leading-none" />
+    
 
-        <div className="border-2 border-black rounded-full px-2 py-1">
+      <div className="border-2 border-black text-center rounded-full w-40 px-2 py-1">
           <BodyText
             text={project.service}
             size="text-xs"
@@ -66,37 +50,26 @@ const ProjectDetail = () => {
         </div>
       </div>
 
-      {/* Full-width image without horizontal paddings */}
+      {/* Cover Image */}
       <img
         src={project.coverImage}
         alt={project.heading}
-        className="w-full h-[600px] object-cover py-10"
+        className="w-full h-[600px] object-cover my-10"
       />
 
+      {/* About Client Section */}
       <div
-        className={`flex flex-col xl:flex-row justify-between items-center gap-8 py-4 xl:py-10 ${theme.layoutPages.paddingHorizontal}`}
+        className={`flex flex-col xl:flex-row justify-between gap-8 py-10 ${theme.layoutPages.paddingHorizontal}`}
       >
-        {/* Left Section (Text) */}
-        <div className="flex flex-col items-start gap-4 flex-1">
-          <Heading
-            text="About the Client"
-            centered={false}
-            lineHeight="leading-loose"
-          />
-
-          <BodyText text={project.details} centered={false} />
-
+        <div className="flex flex-col gap-4 flex-1">
+          <Heading text="About the Client" centered={false} lineHeight="leading-none" />
+          <BodyText text={project.clientDetails} centered={false} />
           <div className="flex flex-row items-center">
-            <BodyText
-              text="Location:"
-              fontWeight="font-semibold"
-              className="mr-2"
-            />
+            <BodyText text="Location:" fontWeight="font-semibold" className="mr-2" />
             <BodyText text={project.location} />
           </div>
         </div>
 
-        {/* Right Section (Image) */}
         <div className="flex-shrink-0">
           <img
             src={project.additionalImages[0]}
@@ -104,66 +77,40 @@ const ProjectDetail = () => {
             className="w-[400px] rounded-md object-cover"
           />
         </div>
-
-
-
-
       </div>
 
-
-
+      {/* Project Description & Technologies */}
       <div
-        className={`flex flex-col xl:flex-row justify-between items-center gap-8 py-4 xl:py-10 ${theme.layoutPages.paddingHorizontal}`}
-      >
-        {/* Left Section (Text) */}
-        <div className="flex flex-col items-start gap-4 flex-1">
-          <Heading
-            text={project.headline}
-            centered={false}
-            lineHeight="leading-loose"
-          />
-
+  className={`flex flex-col xl:flex-row justify-between items-end gap-8 py-10 ${theme.layoutPages.paddingHorizontal}`}
+>
+        {/* Left: Description */}
+        <div className="flex flex-col gap-4 flex-1">
+          <Heading text={project.headline} centered={false} lineHeight="leading-none" />
           <BodyText text={project.details} centered={false} />
-
-     
         </div>
 
-        {/* Techs */}
-
-        {/* vertically align it at bottom i.e. end*/}
-        <div
-        className={`flex flex-col `}
-      >
-    
-        <BodyText
-          text="Tech Used:"
-          centered={false}
-        />
-        
-      <div className="grid grid-cols-2 max-w-3xl gap-4 mt-4 ">
-  {project.technologies.map((tech, index) => {
-    const icon = findTechnologyIcon(tech);
-    return (
-      <Button
-        hoverBgColor="bg-neon"
-        hoverTextColor="text-black"
-        noIconChange={true}
-        key={index}
-        name={tech}
-        icon={icon}
-      />
-    );
-  })}
-</div>
-
+        {/* Right: Tech Stack (bottom aligned) */}
+        <div className="flex flex-col justify-start max-w-4xl items-start gap-2 xl:h-full">
+          <BodyText text="Tech Used:" color="text-grayText" centered={false} fontWeight="font-semibold" />
+          <div className="grid grid-cols-2 max-w-3xl gap-4 mt-2">
+            {project.technologies.map((tech, index) => {
+              const icon = findTechnologyIcon(tech);
+              return (
+                <Button
+                  key={index}
+                  name={tech}
+                  icon={icon}
+                  hoverBgColor="bg-neon"
+                  hoverTextColor="text-black"
+                  noIconChange={true}
+                />
+              );
+            })}
+          </div>
+        </div>
       </div>
 
-
-      </div>
-
-    
-
-      {/* First Carousel: additionalImages[1], [2], [3] */}
+      {/* Carousels */}
       <SplideCarousel
         images={project.additionalImages.slice(1, 4)}
         speed={1}
@@ -171,11 +118,6 @@ const ProjectDetail = () => {
         gap="1rem"
         pauseOnHover={false}
       />
-
-
-      
-
-      {/* Second Carousel: additionalImages[4], [5], [6] */}
       <SplideCarousel
         images={project.additionalImages.slice(4, 7)}
         speed={-1}
@@ -183,17 +125,6 @@ const ProjectDetail = () => {
         gap="1rem"
         pauseOnHover={false}
       />
-
-      {/* Project Features Section */}
-   
-
-      {/* Uncomment if you want the Back to Portfolio link */}
-      {/* <Link
-        to="/portfolio"
-        className="inline-block mt-6 px-4 py-2 bg-blue-500 text-white w-48 rounded-full"
-      >
-        Back to Portfolio
-      </Link> */}
     </div>
   );
 };
