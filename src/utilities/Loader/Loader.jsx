6e30905e20d5545ea149f178438698b61f872129
Omer <engromerbaig@gmail.com>
2025-05-15 20:47:30 +0500

    added

--- src/utilities/Loader/Loader.jsx
+++ src/utilities/Loader/Loader.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import './Loader.css'
+import Heading from '../../components/Heading/Heading';
+import './Loader.css';
 
 const Loader = () => {
-  return     <div className="loader-container bg-neonLight">
-  <div className="loader"></div>
-</div>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center relative">
+        <div className="flex items-baseline">
+          <Heading
+            text="Scons"
+            fontWeight="font-medium"
+            color="text-black"
+            fontFamily="font-coolvetica"
+            size="text-150px md:text-90px"
+            isAnimate={true}
+          />
+          <span
+            className="dot ml-2 w-2 h-2 rounded-full bg-neon pulse"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
