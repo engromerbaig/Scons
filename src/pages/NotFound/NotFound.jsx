7fc6215342eaaacea74dd3f00cd5ec027822d636
Omer <engromerbaig@gmail.com>
2025-05-16 20:03:14 +0500

    added

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { theme } from '../../theme';
 import Heading from '../../components/Heading/Heading';
-import CustomButton from '../../components/CustomButton/CustomButton';
+import Button from '../../components/Button/Button';
 import ScrollToTopLink from '../../utilities/ScrollToTopLink';
 import AnimatedBackground from '../../utilities/AnimatedBackground/AnimatedBackground';
 
@@ -10,12 +10,16 @@ const NotFound = () => {
     <div 
       className={`${theme.layoutPages.paddingVertical} flex h-screen justify-center items-center ${theme.layoutPages.paddingHorizontal} `}
     >
-      <div className="text-center flex flex-col justify-center items-center space-y-8">
+      <div className="text-center flex flex-col justify-center items-center space-y-4">
         <Heading text="404 Error" color="text-neon" size="text-90px" />
         <Heading text="Oops! Page Not Found" color="text-black" size="text-50px" />
         <p className="pb-8 text-black">It looks like the page you’re looking for doesn’t exist.</p>
         <ScrollToTopLink to="/">
-          <CustomButton option="Return to Home" />
+          <Button name="Return to Home" size="text-sm"
+                  textColor="white"
+                  fontWeight="font-bold"
+                  bgColor="bg-black"
+                  className="px-4 py-2 " />
         </ScrollToTopLink>
       </div>
     </div>
