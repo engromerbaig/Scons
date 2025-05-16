e0f251e3e93456969a259a59f7b690b6a225b96e
Omer <engromerbaig@gmail.com>
2025-05-16 15:13:14 +0500

    added

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -5,6 +5,7 @@ import BodyText from "../../components/BodyText/BodyText";
 import Heading from '../../components/Heading/Heading';
 import { theme } from '../../theme';
 import InnerHero from "../../components/InnerHero/InnerHero";
+import contactImage from "../../assets/images/contact.svg";
 
 
 const ContactUs = () => {
@@ -14,6 +15,8 @@ const ContactUs = () => {
   headingText="Get in Contact With Us"
   spanText="Contact"
   bodyText="Have questions or want to work with us? Reach out today — we’re here to help and look forward to connecting with you."
+  illustrationImage={contactImage}
+  illustrationImageWidth="w-3/4"
 />
     </div> );
 }
