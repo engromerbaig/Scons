da5faefce320d5a15ba8962dc54b230f06896e34
Omer <engromerbaig@gmail.com>
2025-05-19 13:04:37 +0500

    empty deliverables

---
+++ src/pages/OurWork/Deliverables.jsx
@@ -0,0 +1,10 @@
+import Heading from "../../components/Heading/Heading";
+import BodyText from "../../components/BodyText/BodyText";
+const Deliverables = () => {
+    return ( <div>
+
+
+    </div> );
+}
+ 
+export default Deliverables;
