a07fc52ffc36a02530ec3e73e01418335a7de368
Omer <engromerbaig@gmail.com>
2025-05-19 13:51:33 +0500

    added

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -3,19 +3,22 @@ import Heading from "../Heading/Heading";
 import VerticalAccordion from "./modules/VerticalAccordion";
 import AnimatedBackground from "../../utilities/AnimatedBackground/AnimatedBackground";
 import { theme } from "../../theme";
+import GreenBelt from "../GreenBelt/GreenBelt";
 
 const Locations = ({isAnimate}) => {
     return (
         <div className={`w-full  h-full   `}>
-            {/* First row - Title section */}
-            <div className={`bg-neon ${theme.layoutPages.paddingHorizontal} py-10`}>
-                <Heading
-                    text="Innovation Centers Across The Globe"
+
+            <GreenBelt>
+   <Heading
+                    text="Our Global Hub's"
                     color="text-black"
                     size="text-50px"
                     centered={true}
                 />
-            </div>
+
+            </GreenBelt>
+    
 
 <VerticalAccordion isAnimate={isAnimate} />
 
