3a4f7ab6366e39de61e8c77141539c4ba1ed2657
Omer <engromerbaig@gmail.com>
2025-05-20 14:56:09 +0500

    created package data

--- src/pages/Packages/PackageCard.jsx
+++ src/pages/Packages/PackageCard.jsx
@@ -1,6 +1,7 @@
 import Heading from "../../components/Heading/Heading";
 import BodyText from "../../components/BodyText/BodyText";
 import Button from "../../components/Button/Button";
+import { packageData } from "./packageData";
 const PackageCard = () => {
     return ( <div>
         Card
---
+++ src/pages/Packages/packageData.js
@@ -0,0 +1,25 @@
+export const packageData = [
+    {
+        id: 1,
+        title: "Basic",
+        price: "5",
+        features: [
+            "Up to 5 users",
+            "Unlimited projects",
+            "Basic analytics",
+            "Limited support",
+        ],
+    },
+
+     {
+        id: 2,
+        title: "Advanced",
+        price: "10",
+        features: [
+            "Up to 5 users",
+            "Unlimited projects",
+            "Basic analytics",
+            "Limited support",
+        ],
+    },
+]
