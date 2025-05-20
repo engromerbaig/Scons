96338ba9ed3f26a2156c171326d120a833e777d7
Omer <engromerbaig@gmail.com>
2025-05-20 16:03:13 +0500

    S

--- src/pages/Packages/PackageCard.jsx
+++ src/pages/Packages/PackageCard.jsx
@@ -13,27 +13,29 @@ const PackageCard = ({ packageInfo }) => {
 
     const convertPrice = (price) => {
         if (currency === "PKR") return price;
-        return (price * currencyRates.GBP).toFixed(2);
+        return (price * currencyRates.GBP).toFixed(0);
     };
 
     return (
-        <div className="relative bg-white border border-grayText rounded-xl shadow-lg py-10 px-12 flex flex-col justify-between h-[600px] overflow-hidden">
+        <div className="relative bg-white gap-y-4 border border-grayText rounded-xl shadow-lg py-10 px-12 flex flex-col justify-between h-[600px] overflow-hidden">
             {/* Title */}
-            <Heading text={packageInfo.title} className="text-3xl font-bold text-center mb-2" />
-              <div className="flex flex-col items-center mb-3">
+            <Heading text={packageInfo.title} size="text-40px" className=" font-bold text-center mb-2" />
+
+            {/* currency part */}
+              <div className="flex flex-row justify-center gap-x-2 items-center mb-3">
                     <div className="text-3xl font-semibold flex items-baseline space-x-1">
-                        <span className="text-base">{currency === "PKR" ? "PKR" : "£"}</span>
-                        <span>{convertPrice(packageInfo.price)}</span>
+                        <span className="text-sm">{currency === "PKR" ? "PKR" : "£"}</span>
+                        <span className="text-60px font-bold">{convertPrice(packageInfo.price)}</span>
                     </div>
-                    <div className="flex mt-2 space-x-2">
+                    <div className="flex mt-2 space-x-1">
                         {["PKR", "GBP"].map((cur) => (
                             <button
                                 key={cur}
                                 onClick={() => setCurrency(cur)}
-                                className={`px-3 py-1 rounded-full text-sm border transition ${
+                                className={`px-1 py-1  text-10px  transition ${
                                     currency === cur
-                                        ? "bg-neon text-white"
-                                        : "bg-gray-100 text-gray-700"
+                                        ? "bg-neon text-black"
+                                        : "bg-gray-100 text-black"
                                 }`}
                             >
                                 {cur}
