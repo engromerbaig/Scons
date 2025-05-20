7cb392391990e8eaf86382b01a9e01654d942c20
Omer <engromerbaig@gmail.com>
2025-05-20 20:41:52 +0500

    packages added

--- src/pages/Packages/Packages.jsx
+++ src/pages/Packages/Packages.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, lazy } from "react";
 import { theme } from "../../theme";
 import PackageCard from "./PackageCard";
 import { packageData } from "./packageData";
@@ -6,20 +6,22 @@ import Heading from "../../components/Heading/Heading";
 import BodyText from "../../components/BodyText/BodyText";
 import StartProjectBelt from "../../components/StartProjectBelt/StartProjectBelt";
 
+const InnerHero = lazy(() => import("../../components/InnerHero/InnerHero"));
+
+
 const Packages = () => {
     return (
-        <div className={`${theme.layoutPages.paddingVertical} ${theme.layoutPages.paddingHorizontal} min-h-screen `}>
-            <div className="flex flex-col pb-10 ">
-            <Heading
-            text="Packages"
-            />
-            <BodyText
-            text="Choose the package that best fits your needs."
-            />
+        <div className={`${theme.layoutPages.paddingBottom}  min-h-screen `}>
 
-            
-            </div>
+                 <InnerHero
+      
+        headingText="Packages"
+        bodyText="Choose the package that best fits your needs."
+        height="h-[50vh]"
+        headingColor="text-black"
+      />
 
+<div className={`${theme.layoutPages.paddingHorizontal} ${theme.layoutPages.paddingVertical}`}>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
   {packageData.map((pkg) => (
                 <PackageCard key={pkg.id} packageInfo={pkg} />
@@ -28,6 +30,10 @@ const Packages = () => {
 
             </div>
 
+
+
+
+</div>
 
 <StartProjectBelt text="Custom Project"/>
 
