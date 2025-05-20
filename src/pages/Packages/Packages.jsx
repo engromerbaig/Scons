27320b3c1ec7ad9a146016453be623d657093341
Omer <engromerbaig@gmail.com>
2025-05-20 17:37:22 +0500

    added

--- src/pages/Packages/Packages.jsx
+++ src/pages/Packages/Packages.jsx
@@ -2,13 +2,34 @@ import { useState } from "react";
 import { theme } from "../../theme";
 import PackageCard from "./PackageCard";
 import { packageData } from "./packageData";
+import Heading from "../../components/Heading/Heading";
+import BodyText from "../../components/BodyText/BodyText";
 
 const Packages = () => {
     return (
-        <div className={`${theme.layoutPages.paddingVertical} ${theme.layoutPages.paddingHorizontal} min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6`}>
-            {packageData.map((pkg) => (
+        <div className={`${theme.layoutPages.paddingVertical} ${theme.layoutPages.paddingHorizontal} min-h-screen `}>
+            <div className="flex flex-col pb-10 ">
+            <Heading
+            text="Packages"
+            />
+            <BodyText
+            text="Choose the package that best fits your needs."
+            />
+
+            
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+  {packageData.map((pkg) => (
                 <PackageCard key={pkg.id} packageInfo={pkg} />
             ))}
+
+
+            </div>
+
+
+
+          
         </div>
     );
 };
