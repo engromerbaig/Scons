874def45999d081bc40065c405f1531b42864ff6
Omer <engromerbaig@gmail.com>
2025-05-21 18:50:41 +0500

    added

--- src/components/BusinessSuccess/BusinessSuccess.jsx
+++ src/components/BusinessSuccess/BusinessSuccess.jsx
@@ -4,7 +4,8 @@ import Heading from "../Heading/Heading";
 import BodyText from "../BodyText/BodyText";
 import './index.css';
 import { theme } from "../../theme";
-import timelineImage from "../../assets/icons/arrow.svg";
+import timelineImage from "../../assets/icons/mobile.png";
+import FadeWrapper from "../../utilities/Animations/FadeWrapper";
 
 const BusinessSuccess = ({heading, spanHeading, bodyText}) => {
   const content = (
@@ -28,19 +29,23 @@ const BusinessSuccess = ({heading, spanHeading, bodyText}) => {
     <div className={`grid  bg-black ${theme.layoutPages.paddingHorizontal}  `}>
       {/* Desktop Layout */}
       <div className="hidden md:grid md:grid-cols-12 min-h-screen ">
-        <div className="col-span-12 md:col-span-8 md:pr-10 flex flex-col justify-center items-start">
+        <div className="col-span-12 md:col-span-6 md:pr-10 flex flex-col justify-center items-start">
           {content}
         </div>
-        <div className="col-span-12 md:col-span-4 h-screen flex items-center justify-center">
+        <div className="col-span-12 md:col-span-6 h-screen flex items-center justify-center">
+          <FadeWrapper>
     <motion.img
       src={timelineImage}
       alt="Timeline Overview"
-      className="w-full h-full object-cover svg-neon" // Changed from object-contain
+      className="w-full h-full object-contain " // Changed from object-contain
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
       loading="lazy"
     />
+
+          </FadeWrapper>
+
   </div>
       </div>
 
