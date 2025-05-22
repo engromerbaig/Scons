3b31317d13326473a0460fb9b5bcad1a2a26c4f1
Omer <engromerbaig@gmail.com>
2025-05-23 01:38:38 +0500

    updated token

--- src/components/Locations/MapComponent.jsx
+++ src/components/Locations/MapComponent.jsx
@@ -37,7 +37,7 @@ const MapComponent = ({ officeCoordinates, accordionData, activeOffice, onPinCli
       ref={mapRef}
     >
       <TileLayer
-        url="https://api.mapbox.com/styles/v1/engromerbaig/cmazsu0x100ct01s64wn7dvsd/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW5ncm9tZXJiYWlnIiwiYSI6ImNtYXpzczRlbjBvbncya3A3NDkxZjE3MDAifQ.aSp6fX0uVkdBt9SQ1VNMTQ"
+        url="https://api.mapbox.com/styles/v1/engromerbaig/cmazsu0x100ct01s64wn7dvsd/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW5ncm9tZXJiYWlnIiwiYSI6ImNtYXp1MDN0djBobXIyaXM2NWNzMGoxdWgifQ.T6bA1hG3-owi57hM4s_DFw"
         attribution='© <a href="https://www.mapbox.com/">Mapbox</a>, © <a href="https://openmaptiles.org/">OpenMapTiles</a>'
       />
       {accordionData.map((office) => (
