343cf47f8c8cbdc53ea33224d27da4f082f31a0e
Omer <engromerbaig@gmail.com>
2025-05-24 04:02:57 +0500

    commented code in ain.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 
-const updateVhVariable = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
+// const updateVhVariable = () => {
+//   const vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// };
 
-window.addEventListener('resize', updateVhVariable);
-window.addEventListener('orientationchange', updateVhVariable);
-updateVhVariable();
+// window.addEventListener('resize', updateVhVariable);
+// window.addEventListener('orientationchange', updateVhVariable);
+// updateVhVariable();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
