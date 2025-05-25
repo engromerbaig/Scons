49939cf3f22f4c6a6218b2ed90c4e586801dac26
Omer <engromerbaig@gmail.com>
2025-05-26 02:12:25 +0500

    added service projects

---
+++ src/pages/ServiceDetails/ServiceProjects.jsx
@@ -0,0 +1,22 @@
+
+
+import React, { useState, useRef, useEffect } from 'react';
+import Heading from '../../components/Heading/Heading';
+import BodyText from '../../components/BodyText/BodyText';
+import { theme } from '../../theme';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+import { IoIosCheckmarkCircle } from 'react-icons/io';
+import { gsap } from 'gsap';
+
+
+
+
+const ServiceProjects = () => {
+    return ( <div>
+
+
+
+    </div> );
+}
+ 
+export default ServiceProjects;
