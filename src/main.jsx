2a484854d9fd1c51ec4fe70a1789a08f7e1a527d
Omer <engromerbaig@gmail.com>
2025-05-27 14:27:44 +0500

    vh height fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 
-// const updateVhVariable = () => {
-//   const vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`);
-// };
+const debounce = (func, wait) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+};
 
-// window.addEventListener('resize', updateVhVariable);
-// window.addEventListener('orientationchange', updateVhVariable);
-// updateVhVariable();
+const updateVhVariable = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+window.addEventListener('resize', debounce(updateVhVariable, 150));
+window.addEventListener('orientationchange', updateVhVariable);
+updateVhVariable();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
