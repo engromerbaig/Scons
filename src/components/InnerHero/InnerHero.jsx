e4284282a3578563e5b29931d646fea61c6cc45c
Omer <engromerbaig@gmail.com>
2025-05-29 13:18:48 +0500

    cleaner version

--- src/components/InnerHero/InnerHero.jsx
+++ src/components/InnerHero/InnerHero.jsx
@@ -1,4 +1,3 @@
-
 import React, { useRef } from "react";
 import Heading from "../Heading/Heading";
 import BodyText from "../BodyText/BodyText";
@@ -12,9 +11,9 @@ const InnerHero = ({
   headingText,
   spanText,
   bodyText,
-  height = "h-screen", // Keep default as h-screen
+  height = "h-screen",
   headingSize = "text-70px",
-  bodySize = "text-25px",// Responsive font sizes
+  bodySize = "text-25px",
   headingColor = "text-black",
   bodyTextColor = "text-black",
   illustrationImageWidth = "w-4/5 md:w-3/5 xl:w-full xl:max-w-lg",
@@ -39,7 +38,6 @@ const InnerHero = ({
         ${showBottomShadow ? "shadow-custom-bottom" : ""}
       `}
     >
-      {/* Optional Pattern Image Overlay */}
       {showPattern && (
         <div
           ref={patternRef}
@@ -49,22 +47,20 @@ const InnerHero = ({
         />
       )}
 
-      {/* Main Content */}
       <div className="relative flex flex-col xl:flex-row items-center justify-between w-full gap-8 z-10 text-white">
         {/* Left Side */}
         <FadeWrapper
           order={1}
-          className="flex flex-col items-start justify-center flex-1 gap-4 text-left min-h-[200px] md:min-h-[250px] xl:min-h-[300px]"
+          className="flex flex-col items-start justify-center flex-1 gap-4 text-left"
         >
-          {/* Logos */}
           {logoImages.length > 0 && (
-            <div className="flex flex-wrap items-center gap-4 mb-2 min-h-[40px]">
+            <div className="flex flex-wrap items-center gap-4 mb-2">
               {logoImages.map((logo, index) => (
                 <img
                   key={index}
                   src={logo}
                   alt={`Logo ${index + 1}`}
-                  className="w-28 aspect-[3/2] object-contain"
+                  className="w-32  object-contain"
                   loading="lazy"
                 />
               ))}
@@ -80,29 +76,29 @@ const InnerHero = ({
             fontWeight="font-black"
             spanFontWeight="font-black"
             centered={false}
-            className="leading-none min-h-[30px] md:min-h-[60px] "
+            className="leading-none"
           />
           <BodyText
             text={bodyText}
             size={bodySize}
             color={bodyTextColor}
             centered={false}
-            className="mt-2 leading-none min-h-[20px] md:min-h-[30px] xl:min-h-[40px]"
+            className="mt-2 leading-none"
           />
-          {children && <div className="mt-4 min-h-[40px]">{children}</div>}
+          {children && <div className="mt-4">{children}</div>}
         </FadeWrapper>
 
         {/* Right Side: Illustration */}
         <FadeWrapper
           order={2}
-          className="flex flex-1 items-center justify-center min-h-[200px] md:min-h-[300px] xl:min-h-[400px]"
+          className="flex flex-1 items-center justify-center"
         >
           {illustrationImage && (
             <img
               src={illustrationImage}
               alt="Hero Illustration"
               className={`${illustrationImageWidth} aspect-[4/3] object-contain`}
-              loading="eager" // Load hero image immediately
+              loading="eager"
             />
           )}
         </FadeWrapper>
