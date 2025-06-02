1c3e7224f381694719dcac2e447713208530def6
Omer <engromerbaig@gmail.com>
2025-06-02 18:15:48 +0500

    just added

--- src/utilities/ReturnButton.jsx
+++ src/utilities/ReturnButton.jsx
@@ -1,14 +1,14 @@
 import Button from "../components/Button/Button";
 import { FaArrowCircleLeft } from "react-icons/fa";
 
-const ReturnButton = () => {
+const ReturnButton = ({text="Portfolio", link="/portfolio"}) => {
   return (
     <div className="fixed bottom-4 left-4 z-[10]">
       <Button
         name={
           <span className="flex items-center gap-2">
             <FaArrowCircleLeft className="text-sm font-black" />
-            Portfolio
+            {text}
           </span>
         }
         type="button"
@@ -16,7 +16,7 @@ const ReturnButton = () => {
         // textColor="black"
         fontSize="text-xs"
         openModal={false}
-        link="/portfolio"
+        link={link}
       />
     </div>
   );
