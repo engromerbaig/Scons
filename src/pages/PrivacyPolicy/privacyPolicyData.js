0b859fe69c61d7425c9c42de04f2121a1f9be8b2
Omer <engromerbaig@gmail.com>
2025-06-04 18:29:19 +0500

    added

--- src/pages/PrivacyPolicy/privacyPolicyData.js
+++ src/pages/PrivacyPolicy/privacyPolicyData.js
@@ -1,8 +1,8 @@
 const privacyPolicyData = {
     title: "Privacy Policy",
     description: [
-        "Welcome to Scons's Privacy Policy. Your privacy is critically important to us.",
-        "This privacy policy document outlines the types of personal information collected and recorded by Scons and how we use it."
+        "Welcome to Scons Tech's Privacy Policy. Your privacy is critically important to us.",
+        "This privacy policy document outlines the types of personal information collected and recorded by Scons Tech and how we use it."
     ],
     policyList: [
         {
