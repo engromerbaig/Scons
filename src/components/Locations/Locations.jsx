189886fcd7dad7e924a2d11518d2f9a28332ddd4
Omer <engromerbaig@gmail.com>
2025-06-12 16:13:01 +0500

    added

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -72,7 +72,7 @@ const Locations = () => {
                 textColor="black"
                 hoverBgColor="bg-neon"
                 hoverTextColor="black"
-                link="https://calendly.com/sconstech-official"
+                link="/schedule-a-meeting"
               />
             </FadeWrapper>
           ) : (
